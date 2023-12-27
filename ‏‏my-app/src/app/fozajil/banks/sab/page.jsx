6865e76ef15d1a5363850c65d727eaabcb72185a
MiladSN2023 <152106448+MiladSN2023@users.noreply.github.com>
2023-12-27 20:57:15 +0300

    Update page.jsx

--- ‏‏my-app/src/app/fozajil/banks/sab/page.jsx
+++ ‏‏my-app/src/app/fozajil/banks/sab/page.jsx
@@ -3,7 +3,7 @@ import styles from './sab.module.css'
 import Image from 'next/image'
 import icon1 from '../../../../../public/11.jpg'
 import { useRef } from 'react'
-
+import { useRouter } from 'next/navigation'
 const Page = () => {
     const username = useRef()
     const password = useRef()
