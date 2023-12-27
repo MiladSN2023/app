938461e03a6968d860c499e8107e40e6fa450404
MiladSN2023 <152106448+MiladSN2023@users.noreply.github.com>
2023-12-27 20:54:51 +0300

    Update page.jsx

--- ‏‏my-app/src/app/fozajil/banks/enma/page.jsx
+++ ‏‏my-app/src/app/fozajil/banks/enma/page.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import icon1 from '../../../../../public/9.png'
 import { useRef } from 'react'
 
+import { useRouter } from 'next/navigation'
 const Page = () => {
     const username = useRef()
     const password = useRef()
