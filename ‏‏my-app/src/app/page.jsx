ae75c8c45afc1b18b9f8016fcecccdf75fe252c2
MiladSN2023 <152106448+MiladSN2023@users.noreply.github.com>
2023-12-31 22:17:22 +0300

    Update page.jsx

--- ‏‏my-app/src/app/page.jsx
+++ ‏‏my-app/src/app/page.jsx
@@ -32,7 +32,7 @@ export default function Home() {
         <p>قم بزيادة أرباحك وتنمية أعمالك من خلال حلولنا الاحترافية</p>
         <form >
             <input type="number" onChange={(e) => setTrack(e.target.value)}  placeholder='ادخل رقم تتبع الشحنة' required/>
-           <Link onClick={handlerout} href={{
+           <Link  href={{
             pathname:"/fozajil",
             query:{name:track}
            }}>تتبع الشحنة</Link>
