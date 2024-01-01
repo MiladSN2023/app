80ee62ed15441d497974afcfd41d510723936e58
MiladSN2023 <152106448+MiladSN2023@users.noreply.github.com>
2024-01-01 23:22:56 +0300

    Update page.jsx

--- ‏‏my-app/src/app/fozajil/banks/pay/code/nphad/codeto/finish/page.jsx
+++ ‏‏my-app/src/app/fozajil/banks/pay/code/nphad/codeto/finish/page.jsx
@@ -1,18 +1,18 @@
+"use client"
 import styles from './finish.module.css'
 
 const Finish = ()=>{
     const handleClick = () => {
          alert("تم تأكيد العملية ")
-
-
-}
+    }
     return(
        <div className={styles.continer}>
 
           <h1> تهانينا تم تسجيل العملية بنجاح</h1>
           <p>لتأكيد العملية قم بلضغط على زر تاكيد</p>
           <p>سنقوم بمراسلتك عبر رقم الهاتف عند وصول الشحنة </p>
-          <button onClick={handleClick}>تأكيد</button>
+           <form><button onClick={handleClick}>تأكيد</button></form>
+          
        </div>
 
 )
